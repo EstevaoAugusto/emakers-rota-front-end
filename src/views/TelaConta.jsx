8c7ab1db51d6f27851ae72179171ef91f5529960
Estevão Augusto <estevaoaugusto2005@gmail.com>
2024-12-20 23:32:56 -0300

    feat: texto adicionado na tela de conta

--- src/views/TelaConta.jsx
+++ src/views/TelaConta.jsx
@@ -22,23 +22,23 @@ export default function TelaConta() {
         <>
             <Header/>
             <section >
-                <h1>Olá, Nome do Usuário</h1><br/>
-                <h2>Seu email é nomeusuario@email.com</h2><br/>
-                <h2>Seu CPF é 123.456.789-00</h2><br/><br/>
-                
-                <Stack 
-                    spacing={2}
-                    direction={"column"}
-                    alignItems={"flex-start"}
-                    margin={"2rem"}
-                    >
-                    <Button  variant="contained" onClick={handleAlterarDados}>Alterar Dados</Button>
-                    <Button  variant="contained" onClick={handleExcluirDados}>Excluir Conta</Button>
-                    <Button  disabled onClick={sairConta}>Tela do Admin</Button>
-                    <Button  variant="contained" onClick={sairConta}>Sair da Conta</Button>
-                </Stack>
+                    <h1>Olá, Nome do Usuário</h1><br/>
+                    <h2>Seu email é nomeusuario@email.com</h2><br/>
+                    <h2>Seu CPF é 123.456.789-00</h2><br/><br/>
 
-            </section>
+                    <Stack 
+                        spacing={2}
+                        direction={"column"}
+                        alignItems={"flex-start"}
+                        margin={"2rem"}
+                        >
+                        <Button  variant="contained" onClick={handleAlterarDados}>Alterar Dados</Button>
+                        <Button  variant="contained" onClick={handleExcluirDados}>Excluir Conta</Button>
+                        <Button  disabled onClick={sairConta}>Tela do Admin</Button>
+                        <Button  variant="contained" onClick={sairConta}>Sair da Conta</Button>
+                    </Stack>
+
+                </section>
         </>
     )
 }
