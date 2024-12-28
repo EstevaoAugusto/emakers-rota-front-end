67805e001097ff8369e3e20a41d450707912e48f
Estevão Augusto <estevaoaugusto2005@gmail.com>
2024-12-28 00:17:45 -0300

    feat: adicionado funcionalidade de cadastro

--- src/views/TelaCadastro.jsx
+++ src/views/TelaCadastro.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import styles from "../style/Cadastro.module.css"
 import LoginCadastro from "../assets/images/IlustracaoLoginCadastro.png";
 
+
 const Cadastro = () => {
     const [usuario, setUsuario] = useState(     // Informações de um usuário inicialmente vazio 
         {                                       // É preciso realizar o cadastro de um usuario
@@ -13,34 +14,95 @@ const Cadastro = () => {
             Senha: "",
             SenhaConfirmacao: "",
         }
-    )
+    );
+    const [erros, setErros] = useState(
+        {
+            erroNome: "",
+            erroCPF: "",
+            erroEmail: "",
+            erroSenha: "",
+            erroSenhaConfirmacao: "",
+        }
+    );
+    const [valorHTML, setValorHTML] = useState(     
+        {                                       
+            Nome: "",
+            CPF: "",
+            Email: "",
+            Senha: "",
+            SenhaConfirmacao: "",
+        }
+    );
+
+    const handleChange = (e) => {
+        console.log(valorHTML)
+        const { name, value } = e.target;
+        setValorHTML({ ...valorHTML, [name]: value });
+    };
 
     let navegador = useNavigate(); // trata da navegação
 
-    
-
     const navegarLogin = () => {
-        navegador("/TelaLogin")
-    };
+        navegador("/TelaLogin");
+        
+    };        
+
+    const handleRegistrar = () => {
+        const novosErros = { Nome: "", CPF: "", Email: "", Senha: "", SenhaConfirmacao: "",};
+
+        if (!valorHTML.Nome || !valorHTML.CPF || !valorHTML.Email || !valorHTML.Senha || !valorHTML.SenhaConfirmacao) {
+            window.alert("Uma das caixas de texto não foi preenchida");
+            return;
+        }
+        if(valorHTML.CPF.length < 12){
+            novosErros.CPF = "Seu cpf é menor do que 12 digitos";
+        } 
+        if(!validarEmail(valorHTML.Email)){
+            novosErros.Email = "Este email é invalido."
+        }
+        if(valorHTML.Senha.length < 8){
+            novosErros.Senha = "A senha informada é menor do que 8 digitos. Digite outro.";
+        }
+        if(valorHTML.Senha != valorHTML.SenhaConfirmacao){
+            novosErros.SenhaConfirmacao = "A senha de confirmação não é igual a senha anterior.";
+        }
+
+        setErros(novosErros);
+
+        // Verifica se há erros antes de registrar
+        const temErros = Object.values(novosErros).some((erro) => erro !== "");
+        if (!temErros) {
+            // Simula o cadastro do usuário
+            setUsuario(valorHTML);
+            console.log(usuario)
+            window.alert("Usuário cadastrado com sucesso!");
+        }
+    }
+
+    const validarEmail = (email) => {
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        return regex.test(email);
+    }
 
     return(
         <>
             {/* Conjunto de caixas de textos a serem preenchidas. 
                 O valor digitado nelas resultara na criação de um novo usuário.*/}
             <section className={styles.secaoCadastro}>
-                <div>
-                <h1>Registrar</h1><br/>
-                <TextField required id="outlined-basic" label="Nome" value={usuario.Nome} variant="outlined"/><br/>
+                <form action="">
+                    <h1>Registrar</h1><br/>
                 
-                <TextField required id="outlined-basic" label="CPF" value={usuario.CPF} variant="outlined" /><br/>
-                <TextField required id="outlined-basic" label="Email" value={usuario.Email} variant="outlined" /><br/>
-                <TextField required id="outlined-basic" label="Senha" value={usuario.Senha} variant="outlined" /><br/>
-                <TextField required id="outlined-basic" label="Confirmar Senha" value={usuario.SenhaConfirmacao} variant="outlined" /><br/>
-                {/* Cadastra um novo usuário.*/}
-                <Button variant="contained">Cadastrar</Button><br/>
-                {/* Realiza login de usuário*/}
-                <Button variant="text" onClick={navegarLogin}>Fazer Login</Button>
-                </div>
+                    <TextField required helperText={erros.erroNome} label="Nome" name="Nome" value={valorHTML.Nome} onChange={handleChange} variant="outlined"/><br/>
+                    <TextField required helperText={erros.erroCPF} label="CPF" name="CPF" value={valorHTML.CPF} onChange={handleChange} variant="outlined" slotProps={{ htmlInput: { maxLength: 12,  /* Limita o número de caracteres para 50*/ }}}/><br/>
+                    <TextField required helperText={erros.erroEmail} label="Email" name="Email" value={valorHTML.Email} onChange={handleChange} variant="outlined" /><br/>
+                    <TextField required helperText={erros.erroSenha} type="password" label="Senha" name="Senha" value={valorHTML.Senha} onChange={handleChange} variant="outlined" /><br/>
+                    <TextField required helperText={erros.erroSenhaConfirmacao} type="password" label="SenhaConfirmacao" name="SenhaConfirmacao" value={valorHTML.SenhaConfirmacao} onChange={handleChange} variant="outlined" /><br/>
+                    {/* Cadastra um novo usuário.*/}
+                    <Button variant="contained" onClick={handleRegistrar}>Cadastrar</Button><br/>
+                    {/* Realiza login de usuário*/}
+                    <Button variant="text" onClick={navegarLogin}>Fazer Login</Button>
+                </form>
 
                 <div>
                     <img src={LoginCadastro} alt="Ilustração Login Cadastro"/>
