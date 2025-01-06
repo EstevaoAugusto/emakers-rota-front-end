235ced143fb33e4bf5ea3aabec7f197e755aa4a9
Estevão Augusto <estevaoaugusto2005@gmail.com>
2025-01-05 22:04:51 -0300

    style: adicionado estilização da tela de administrador.

--- src/views/TelaAdministrador.jsx
+++ src/views/TelaAdministrador.jsx
@@ -1,10 +1,38 @@
 import Header from "../components/Header.jsx"
+import styles from "../style/Administrador.module.css";
+import { Button ,TextField } from "@mui/material";
 
 export default function TelaAdministrador(){
 
     return (
         <>
             <Header/>
+
+            <section>
+                <main className={styles.containerGrid}>
+                    <form action="" className={`${styles.formulario} ${styles.formularioCor1}`}>
+                        <h1 className={styles.tituloH1}>Cadastrar Jogo</h1>
+                        <TextField required  label="Titulo do Jogo" name="TituloJogo"  variant="outlined"/>
+                        <TextField required  label="Genero" name="Genero"  variant="outlined"/>
+                        <TextField required  label="Descricao" name="Descricao"  variant="outlined"/>
+                        <Button variant="contained">Inserir Imagem</Button>
+                        <Button variant="contained">Cadastrar</Button>
+                    </form>
+                    <form action="" className={`${styles.formulario} ${styles.formularioCor2}`}>
+                        <h1 className={styles.tituloH1}>Remover Jogo</h1>
+                        <TextField required  label="Titulo do Jogo ou ID" name="Titulo do Jogo ou ID"  variant="outlined"/>
+                        <Button variant="contained">Remover</Button>
+                    </form>
+                    <form action="" className={`${styles.formulario} ${styles.formularioCor1}`}>
+                        <h1 className={styles.tituloH1}>Editar Jogo</h1>
+                        <TextField required  label="Titulo do Jogo" name="TituloJogo"  variant="outlined"/>
+                        <TextField required  label="Genero" name="Genero"  variant="outlined"/>
+                        <TextField required  label="Descricao" name="Descricao"  variant="outlined"/>
+                        <Button variant="contained">Inserir Imagem</Button>
+                        <Button variant="contained">Cadastrar</Button>
+                    </form>
+                </main>
+            </section>
         </>
     )
 }
