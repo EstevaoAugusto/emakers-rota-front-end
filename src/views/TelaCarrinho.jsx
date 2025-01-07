62aea54161a664aef6278371d4f5825b1e911667
Estevão Augusto <estevaoaugusto2005@gmail.com>
2025-01-07 00:41:44 -0300

    feat: adicionado conteudo da pagina Carrinho
    Era para ter dado git add em todos, mas vi que as mudanças eram todas da mesma pagina depois que fiz o commit. Oops.

--- src/views/TelaCarrinho.jsx
+++ src/views/TelaCarrinho.jsx
@@ -1,7 +1,15 @@
 import Header from "../components/Header.jsx";
 import styles from "../style/Carrinho.module.css";
+import JogoItemCarrinho from "../components/JogoItemCarrinho.jsx";
+import { useState } from "react";
+import { FormControl, FormControlLabel, FormLabel, RadioGroup, Radio, Button } from "@mui/material";
 
 export default function TelaCarrinho() {
+    const [pagamento, setPagamento] = useState("PIX");
+
+    const handleChange = (event) => {
+        setPagamento(event.target.value);
+    };
 
     const estiloH1_1 = {
         fontSize: "36px",
@@ -23,10 +31,31 @@ export default function TelaCarrinho() {
                     <div className={styles.containerGrid}>
                         <div className={styles.containerFlex1}>
                             <h1 style={estiloH1_1}>Carrinho de Compras</h1>
-
+                            <JogoItemCarrinho/>
                         </div>
                         <div className={styles.containerFlex2}>
-                            <h1 style={estiloH1_2} >Método de Pagamento</h1>
+                            <FormControl>
+                                    <FormLabel id="demo-controlled-radio-buttons-group">Método de Pagamento</FormLabel>
+                                    <RadioGroup
+                                    aria-labelledby="demo-controlled-radio-buttons-group"
+                                    name="controlled-radio-buttons-group"
+                                    value={pagamento}
+                                    >
+                                        <img src="src/assets/images/PagamentoPix.png" alt="Icone do PIX, uma forma de pagamento" className={styles.imagemPix}/>
+                                        <FormControlLabel value="PIX" control={<Radio />} label="PIX" />
+                                    </RadioGroup>
+                            </FormControl>
+                            <FormControl>
+                                    <FormLabel id="demo-controlled-radio-buttons-group">Método de Pagamento</FormLabel>
+                                    <RadioGroup
+                                    aria-labelledby="demo-controlled-radio-buttons-group"
+                                    name="controlled-radio-buttons-group"
+                                    value={pagamento}
+                                    >
+                                        <FormControlLabel value="PIX" control={<Radio />} label="Eu aceito os termos e condições" />
+                                    </RadioGroup>
+                            </FormControl>
+                            <Button variant="text" >Finalizar Compra</Button>
 
                         </div>
                     </div>
