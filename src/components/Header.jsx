c7c2b9a7f5c4e253e07f17c2f448a39dab90102f
Estevão Augusto <estevaoaugusto2005@gmail.com>
2025-01-08 23:31:45 -0300

    fix: corrijido icone de hamburguer virar X

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,43 @@
 import { Button, TextField, InputAdornment } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import styles from "../style/Header.module.css"
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"; 
+import IconButtonComponent from "./IconButtonComponent";
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false); // estado do menu
+                                                     // varia de aberto(TRUE) e fechado (FALSE)
     let navegador = useNavigate(); // trata da navegação
 
-    const navegar = (rota) =>{
+    const navegar = (rota) =>{ 
         navegador(rota);
     }
 
     const trocarMenu = () => {
-        setMenuOpen((estadoAnterior) => !estadoAnterior);
+        setMenuOpen((estadoAnterior) => !estadoAnterior); // troca o estado do menu
     }
 
-    useEffect(() => {
-        if(menuOpen){
-            document.body.style.overflow = "hidden"; // Desabilita o scroll
-        } else {
-            document.body.style.overflow = "auto"; // Habilita o scroll
+    useEffect(() => { //Esse useEffect é acionado toda vez que o menuOpen trocar de estado
+        if(menuOpen){ 
+            document.body.style.overflow = "hidden"; // Caso esteja aberto, desabilitar scroll
+            
+        } else { 
+            document.body.style.overflow = "auto"; // Senao, habilita scroll
         }
     }, [menuOpen]);
 
     return (   
         <header>
-            <nav className={`${styles.flexContainer} ${ menuOpen ? styles.mobile : ""}`}>
+            <nav className={`${styles.flexContainer} ${ menuOpen ? styles.mobile : ""}`}> {/* Quando menuOpen é verdadeiro, certas estilizações sao ativadas para garantir a responsividade*/}
                 <img className={`${styles.imagemLogo} ${menuOpen ? styles.hidden: ""}`} src="src\assets\images\Logo.png" onClick={() => navegar("/TelaPrincipal")}></img>
             
                 {/* Conjunto de botoes. 
                 Em teoria, cada um deles leva a uma pagina do site diferente.
                 Na realidade, apenas alguns te levaram a uma pagina. */}
 
-                
                     <div className={`${styles.exibirOpcao} ${ menuOpen ? styles.hamburguer : ""}`}>
                         <Button className={styles.botao} variant="text" >Lançamentos</Button>
                     </div>
@@ -55,16 +59,31 @@ const Header = () => {
                     </div>
 
                     <div className={`${styles.exibirOpcao} ${ menuOpen ? styles.hamburguer : ""}`}>
-                        <TextField className={styles.caixaTexto} id="outlined-basic" label=""  variant="outlined" />
+                        <TextField className={styles.caixaTexto} id="outlined-basic" label="" variant="outlined"
+                        slotProps={{
+                            input: {
+                                endAdornment:
+                                <InputAdornment position="end">
+                                    <SearchIcon/>
+                                </InputAdornment>
+                            },
+                          }}
+                        />
                     </div>
                 
                 {/* Menu hambúrguer */}
                 
                 <div className={`${styles.menuSection} ${ menuOpen ? styles.on : ""}`}>
                     <div className={styles.menuToggle} onClick={trocarMenu}>
-                        <div className={`${styles.line1} ${ menuOpen ? styles.changed : "" }`}> </div>
-                        <div className={`${styles.line2} ${ menuOpen ? styles.changed : "" }`}> </div>
-                        <div className={`${styles.line3} ${ menuOpen ? styles.changed : "" }`}> </div>
+                        { !menuOpen ? //Caso o menu nao tenha sido aberto, deixar o icone hamburgue
+                            <IconButtonComponent Icone={<MenuIcon/>}
+                                border={"0"} color={"#4B626C"} backgroundColor={"none"}
+                            /> 
+                            : //Senao coloque o icone de fechar
+                            <IconButtonComponent Icone={<CloseIcon/>}
+                                border={"0"} color={"#FFFFFF"} backgroundColor={"none"} 
+                            />
+                        }
                     </div>
                 </div>
             </nav>
