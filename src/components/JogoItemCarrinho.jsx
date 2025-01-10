e8c85708a366cc1a4e059b29b83d8af5e3741706
Estevão Augusto <estevaoaugusto2005@gmail.com>
2025-01-10 01:44:23 -0300

    refactor: alteraçoes de modo de estilização

--- src/components/JogoItemCarrinho.jsx
+++ src/components/JogoItemCarrinho.jsx
@@ -1,63 +1,47 @@
+import styles from "../style/JogoCarrinho.module.css";
+
 
 const JogoItemCarrinho = () => {
-
-
-
-    const styleContainerGrid = {
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr',
-    };
-
-    const styleImagem = {
-        height: '250px',
-        width: '190px',
-    };
-
-    const styleContainerFlex = {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-    };
 
     const jogos = [
         { caminhoImagem: "src/assets/images/posters/BattleForBikiniBottom.png", 
             nomeJogo: "Sponge Bob: Battle for Bikini Bottom", 
-            preco: 299,
+            preco: 299.00,
             plataformas: "PC"
         }, 
         { caminhoImagem: "src/assets/images/posters/DeathStranding.png", 
             nomeJogo: "Death Stranding",
-            preco: 299,
+            preco: 299.00,
             plataformas: "PC",
         },
     ];
     
-    const precoTotal = jogos.reduce((soma, produto) => soma + produto.preco, 0);
+    const precoTotal = jogos.reduce((soma, produto) => soma + produto.preco, 0); //calcula o preco total dos jogos
 
     return(
-        <section>
+        <section className={styles.styleContainerFlexJogoItem}>
             {jogos.map((jogo) => {
                 return(
-                    <div style={styleContainerGrid} key={jogo.nomeJogo}>
+                    <div className={styles.styleContainerGrid} key={jogo.nomeJogo}>
                         <div>
-                            <div style={styleContainerFlex}>
+                            <div className={styles.styleContainerFlex}>
                                 <p>{jogo.nomeJogo}</p>
                                 <p>Plataforma: {jogo.plataformas}</p>
-                                <p>R${jogo.preco.toFixed(2)}</p>
+                                <p className={styles.fontTamanho}>R${jogo.preco.toFixed(2)}</p>
                             </div>
                         </div>
                         <div>
-                            <img src={jogo.caminhoImagem} alt={`Jogo ${jogo.nomeJogo}`} style={styleImagem} />
+                            <img src={jogo.caminhoImagem} alt={`Jogo ${jogo.nomeJogo}`} className={styles.styleImagem} />
                         </div>
                     </div>
                 )            
             })}
-            <div style={styleContainerGrid}>
+            <div className={styles.styleContainerGridPrecoTotal}>
                 <div>
-                    <p>R${precoTotal.toFixed(2)}</p>
+                    <p className={styles.fontTamanho}>R${precoTotal.toFixed(2)}</p>
                 </div>
                 <div>
-                    <p>Total</p>
+                    <p className={styles.fontTamanho}>Total</p>
                 </div>
             </div>
         </section>
